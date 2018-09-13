46cb0c7ab06cb8eeb265b8f5c8024ba4ad650ccf
samanthamazzola <smazzola7@gmail.com>
2018-09-13 14:40:59 -0400

    added actions to Builds Contr

--- GC-Final/Controllers/BuildsController.cs
+++ GC-Final/Controllers/BuildsController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using GC_Final.Models;
 
 namespace GC_Final.Controllers
 {
+    [Authorize]
     public class BuildsController : Controller
     {
         // GET: Builds
@@ -14,11 +17,38 @@ namespace GC_Final.Controllers
             return View();
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(Build UserBuild)
         {
-            return View();
+            PCEntities ORM = new PCEntities();
+            ORM.Builds.Add(UserBuild);
+            ORM.SaveChanges();
+            ViewBag.UserBuild = UserBuild;
+
+            return RedirectToAction("_Edit", UserBuild.BuildID);
         }
 
+        //edit when given a buildID
+        private ActionResult _Edit(string BuildID)
+        {
+            PCEntities ORM = new PCEntities();
+
+            ViewBag.UserBuild = ORM.Builds.Where(x => x.BuildID == BuildID).ToList()[0];
+            
+            return View("Edit");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            PCEntities ORM = new PCEntities();
+
+            if(ORM.Builds.Where(x => x.BuildID == id).ToArray()[0].OwnerID == User.Identity.GetUserId())
+            {
+                return RedirectToAction("_Edit", id);
+            }
+            return RedirectToAction("Display", id);
+        }
+
+        [AllowAnonymous]
         public ActionResult Display()
         {
             return View();
