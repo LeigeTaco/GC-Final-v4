bda6618d00fe8733cbace42ac7cf1668cba25181
Your Name <turnerp81@gmail.com>
2018-09-13 21:58:03 -0400

    .

--- GC-Final/Controllers/HomeController.cs
+++ GC-Final/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace GC_Final.Controllers
     {
         public ActionResult Index()
         {
+            ViewPag.GPUs = PullGPUs();
             return View();
         }
 
@@ -29,9 +30,60 @@ namespace GC_Final.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = PullGPUs();
 
             return View();
+        }
+        [HttpGet]
+        private List<JObject> PullGPUs()
+        {
+            JObject jsoninfo;
+            string info;
+            //for (int i = 1; i < 9; i++) //NEEDS- add multiple page queries
+            //{
+            HttpWebRequest apiRequest = WebRequest.CreateHttp($"https://api.zinc.io/v1/search?query=GPU&page=1&retailer=amazon");
+            //string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(ConfigurationManager.AppSettings["apizinc"]));
+            Request.Headers.Add("Authorization", ConfigurationManager.AppSettings["ZINCKey"]);
+
+            apiRequest.Headers.Add("-u", ConfigurationManager.AppSettings["apizinc"]);
+            apiRequest.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)";
+
+
+            HttpWebResponse apiResponse = (HttpWebResponse)apiRequest.GetResponse();
+            //if (apiResponse.StatusCode != HttpStatusCode.OK) //http error 200
+            //{
+            //    return (error);
+            //}
+
+            StreamReader responseData = new StreamReader(apiResponse.GetResponseStream());
+
+            info = responseData.ReadToEnd();
+
+            jsoninfo = JObject.Parse(info);
+
+            List<JObject> GPUs = new List<JObject>();
+
+            for (int i = 0; i <= 14; i++)
+            {
+                string x = jsoninfo["results"][i]["product_id"].ToString();
+
+                HttpWebRequest apiRequest1 = WebRequest.CreateHttp($"https://api.zinc.io/v1/products/{x}?retailer=amazon");
+                apiRequest1.Headers.Add("Authorization", ConfigurationManager.AppSettings["apizinc"]); //used to add keys
+                apiRequest1.Headers.Add("-u", ConfigurationManager.AppSettings["apizinc"]);
+                apiRequest1.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)";
+
+                //NEEDS - Add if apiresponse error
+
+                StreamReader responseData1 = new StreamReader(apiResponse.GetResponseStream());
+
+                string gpuinfo = responseData1.ReadToEnd();
+
+                JObject Temp = JObject.Parse(gpuinfo);
+
+                GPUs.Add(Temp);
+            }
+
+            return GPUs;
         }
 
     }
