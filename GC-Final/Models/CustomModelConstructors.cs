8ca23e966ab5bed96fdc0a46a1a0bb2c5cf4d8a0
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-09-14 11:35:27 -0400

    this was the fastest 145 lines of code I've ver written

--- GC-Final/Models/CustomModelConstructors.cs
+++ GC-Final/Models/CustomModelConstructors.cs
@@ -5,7 +5,139 @@ using System.Web;
 
 namespace GC_Final.Models
 {
-    public class CustomModelConstructors
+    public partial class Case
     {
+        public Case(string titleFromAPI)
+        {
+            CaseID = Guid.NewGuid().ToString("D");
+            title = titleFromAPI;
+        }
+    }
+
+    public partial class CPU
+    {
+        public CPU(string titleFromAPI)
+        {
+            CPUID = Guid.NewGuid().ToString("D");
+            title = titleFromAPI;
+        }
+    }
+
+    public partial class GPU
+    {
+        public GPU(string titleFromAPI)
+        {
+            GPUID = Guid.NewGuid().ToString("D");
+            title = titleFromAPI;
+        }
+    }
+
+    public partial class HardDrive
+    {
+        public HardDrive(string name)
+        {
+            HardDriveID = Guid.NewGuid().ToString("D");
+            product_id = "xxxxxxxxxx";
+            @interface = " ";
+            size = true;
+            capacity = " ";
+            speed = " ";
+            product_description = " ";
+            brand = " ";
+            price = 0;
+            stars = " ";
+            main_image = new byte[] { 0, 0, 0, 0 };
+            manufacturer = " ";
+        }
+    }
+
+    public partial class Monitor
+    {
+        public Monitor(string name)
+        {
+            MonitorID = Guid.NewGuid().ToString("D");
+            //title = name;
+        }
+    }
+
+    public partial class Motherboard
+    {
+        public Motherboard(string name)
+        {
+            MotherboardID = Guid.NewGuid().ToString("D");
+            title = name;
+        }
+    }
+
+    public partial class OpticalDriver
+    {
+        public OpticalDriver(string name)
+        {
+            OpticalDriverID = Guid.NewGuid().ToString("D");
+            product_id = "xxxxxxxxxx";
+            rewriteable = true;
+            drive_type = " ";
+            read_speed = 0;
+            write_speed = 0;
+            wattage = 0;
+            type = " ";
+            title = name;
+            product_description = " ";
+            brand = " ";
+            price = 0;
+            stars = " ";
+            main_image = new byte[] { 0, 0, 0, 0 };
+            manufacturer = " ";
+        }
+    }
+
+    public partial class PCICard
+    {
+        public PCICard(string name)
+        {
+            PCIcardID = Guid.NewGuid().ToString("D");
+            product_description = " ";
+            title = name;
+            product_id = "xxxxxxxxxx";
+            brand = " ";
+            price = 0;
+            stars = " ";
+            main_image = new byte[] { 0, 0, 0, 0 };
+            manufacturer = " ";
+        }
+    }
+
+    public partial class Peripheral
+    {
+        public Peripheral(string name)
+        {
+            PeripheralsID = Guid.NewGuid().ToString("D");
+            product_id = "xxxxxxxxxx";
+            title = name;
+            product_description = " ";
+            brand = " ";
+            price = 0;
+            stars = " ";
+            main_image = new byte[] { 0, 0, 0, 0 };
+            manufacturer = " ";
+        }
+    }
+
+    public partial class PSU
+    {
+        public PSU(string name)
+        {
+            PSUID = Guid.NewGuid().ToString("D");
+            title = name;
+        }
+    }
+
+    public partial class RAM
+    {
+        public RAM(string name)
+        {
+            RAMID = Guid.NewGuid().ToString("D");
+            title = name;
+        }
     }
 }
