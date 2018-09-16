047f06f5bba6ecf79291316c77bde42e19b95254
williamTwomey <ashketchup.pkmn@gmail.com>
2018-09-16 00:42:23 -0400

    Added the Display User view and went to bed.

--- GC-Final/Controllers/UsersController.cs
+++ GC-Final/Controllers/UsersController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GC_Final.Models;
 
 namespace GC_Final.Controllers
 {
     public class UsersController : Controller
     {
         // GET: Users
-        public ActionResult Index()
+        public ActionResult Display(string id)
         {
+            Entities ORM = new Entities();
+            //ViewBag.Username = 
+
             return View();
         }
 
