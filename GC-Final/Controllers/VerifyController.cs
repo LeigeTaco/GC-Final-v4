86bfaa2c527456802bf630e3ba52fcd6d59f0eef
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-09-18 17:10:26 -0400

    Almost finished validation method. Still needs AJAX implementation.

---
+++ GC-Final/Controllers/VerifyController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GC_Final.Models;
+
+namespace GC_Final.Controllers
+{
+    public class VerifyController : ApiController
+    {
+
+        [HttpPost]
+        public Dictionary<string, string> CheckCompataibility(string motherboard_id, string cpu_id, string psu_id, string case_id, string gpu_id, byte gpu_count, string[] ram_ids, string[] harddrive_ids, string[] optical_ids, string[] pci_ids, string[] peripheral_ids, string[] monitor_ids)
+        {
+            Entities ORM = new Entities();
+
+            Build temp = new Build();
+            temp.Motherboard = ORM.Motherboards.Find(motherboard_id);
+            temp.CPU = ORM.CPUs.Find(cpu_id);
+            temp.PSU = ORM.PSUs.Find(psu_id);
+            temp.PCCase = ORM.PCCases.Find(case_id);
+            temp.GPU = ORM.GPUs.Find(gpu_id);
+            temp.GPUCount = gpu_count;
+            temp.BuildsRAMs = new List<BuildsRAM>();
+
+            List<RAM> _tempR = new List<RAM>();
+            foreach (string s in ram_ids)
+            {
+                _tempR.Add(ORM.RAMs.Find(s));
+            }
+            RAM[] tempR = _tempR.ToArray();
+
+            List<PCICard> _tempP = new List<PCICard>();
+            foreach (string s in pci_ids)
+            {
+                _tempP.Add(ORM.PCICards.Find(s));
+            }
+            PCICard[] tempP = _tempP.ToArray();
+
+            List<Peripheral> _tempPE = new List<Peripheral>();
+            foreach (string s in peripheral_ids)
+            {
+                _tempPE.Add(ORM.Peripherals.Find(s));
+            }
+            Peripheral[] tempPE = _tempPE.ToArray();
+
+
+
+            return temp.GetCompat();
+        }
+
+    }
+}
